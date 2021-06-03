110ce4107b826542b45375724ac3d72bf02e60a6
Gandalf The Blue <sami_dimi@abv.bg>
2021-06-03 11:55:10 +0300

    Merge remote-tracking branch 'origin/main' into main

--- THE GAME/src/com/elsys/Coordinates.java
+++ THE GAME/src/com/elsys/Coordinates.java
@@ -9,6 +9,7 @@ public class Coordinates {
     }
 
     public int getX() {return X;}
-
+    public void setX(int x) {X = x;}
     public int getY() {return Y;}
+    public void setY(int y) {Y = y;}
 }
--- THE GAME/src/com/elsys/Map.java
+++ THE GAME/src/com/elsys/Map.java
@@ -4,22 +4,48 @@ import java.util.TreeMap;
 
 public class Map {
     TreeMap<Coordinates,GameObject> map = new TreeMap<>();
+    final int max_X = 99;
+    final int min_X = 0;
+    final int max_Y = 99;
+    final int min_Y = 0;
     Coordinates player_coord;
     Player player;
     Map(){
         this.player_coord = new Coordinates( 1, 1); //to-do
         this.player = new Player();
     }
+
     void move_up(){
-        //to-do
+        if (player_coord.getY() < max_Y){
+            map.replace(player_coord, new EmptySpace());
+            int Y = player_coord.getY();
+            player_coord.setY(++Y);
+            map.replace(player_coord, player);
+        }
     }
     void move_down(){
-        //to-do
+        if (player_coord.getY() > min_Y){
+            map.replace(player_coord, new EmptySpace());
+            int Y = player_coord.getY();
+            player_coord.setY(--Y);
+            map.replace(player_coord, player);
+        }
     }
     void move_left(){
-        //to-do
+        if (player_coord.getX() > min_X){
+            map.replace(player_coord, new EmptySpace());
+            int X = player_coord.getX();
+            player_coord.setX(--X);
+            map.replace(player_coord, player);
+        }
     }
-    void move_write(){
-        //to-do
+    void move_right(){
+        if (player_coord.getY() < max_X){
+            map.replace(player_coord, new EmptySpace());
+            int X = player_coord.getX();
+            player_coord.setX(++X);
+            map.replace(player_coord, player);
+        }
     }
+
 }
