34f06adfa51f113a374fd0e2eb30c484ebccc000
NikolayDinkov <nikdinkov@abv.bg>
2021-06-06 16:27:23 +0300

    For Ribcho

--- THE GAME/src/com/elsys/EndHole.java
+++ THE GAME/src/com/elsys/EndHole.java
@@ -7,4 +7,11 @@ public class EndHole implements GameObject{
     public EndHole(Coordinates our_coords) {
         this.coords = our_coords;
     }
+
+    void end_quest(Player player){ // not sure if this will be all in the final
+        if(player.getAmountKeys() == 5) {
+            System.out.println("You won");
+        }
+    }
+
 }
---
+++ THE GAME/src/com/elsys/Rock_Wall.java
@@ -0,0 +1,7 @@
+package com.elsys;
+
+public class Rock_Wall extends Item{ // needed for the walls of the map
+    Rock_Wall(String name) {
+        super(name, false);
+    }
+}
